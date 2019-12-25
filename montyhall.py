75f14db80f67f4923bb989ca7df4acccbbd16478
facuevas <franciscuevas4@gmail.com>
2019-12-25 01:25:23 -0800

    second commit

--- montyhall.py
+++ montyhall.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+'''
+Proving the Monty Hall Problem with Python.
+
+Assumptions for Monty Hall problem (based on https://en.wikipedia.org/wiki/Monty_Hall_problem):
+1) The host must always open a door that was not picked by the contestant
+2) The host must always open a door to reveal a got and never the car
+3) The host must always offer the chance to switch between the originally chosen door and the remaining closed door
+'''
+
+def main():
+    #TODO
+    #Create a way to randomize "door list"
+    #Create a way to command to let player select door
+    #Create a way to look for door that is not player selected or contains car
+    #Have real time stats
+    print("MONTY HALL")
+
+if __name__ == "__main__":
+    main() 
